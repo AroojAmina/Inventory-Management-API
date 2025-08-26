812a39b09c59b579030cefd5f770eef682b64d17
Arooj Amina <aroojamina82@gmail.com>
2025-08-26 17:23:58 +0500

    added migrations for user and permisssion tabel

--- inventory-ecommerce-api/app/core/models.py
+++ inventory-ecommerce-api/app/core/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.utils.db_utils import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class CartItem(db.Model):
@@ -135,15 +136,38 @@ class ReturnProduct(db.Model):
     def __repr__(self):
         return f'<ReturnProduct {self.id} - Product ID: {self.product_id}, Quantity: {self.quantity}>'
     
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(50), default='customer')
+    is_active = db.Column(db.Boolean(), default=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    customer = db.relationship('Customer', back_populates='user', uselist=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
 class Customer(db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     phone = db.Column(db.String(20), nullable=True)
     is_trash = db.Column(db.Boolean(), default=False)
     
+    user = db.relationship('User', back_populates='customer')
     carts = db.relationship('Cart', back_populates='customer')
     transactions = db.relationship('Transaction', back_populates='customer')
 
@@ -164,3 +188,13 @@ class StockMovement(db.Model):
 
     def __repr__(self):
         return f'<StockMovement {self.id} - Product ID: {self.product_id}, Change: {self.quantity_change}>'
+
+class Permission(db.Model):
+    __tablename__ = 'permissions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+
+    def __repr__(self):
+        return f'<Permission {self.name}>'
