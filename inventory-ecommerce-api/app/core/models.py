673d6d9432d2a923d2b8ccbba073d5b588c0e32d
Arooj Amina <aroojamina82@gmail.com>
2025-08-27 15:32:25 +0500

    add passwared value 128 to 255

--- inventory-ecommerce-api/app/core/models.py
+++ inventory-ecommerce-api/app/core/models.py
@@ -141,7 +141,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(50), default='customer')
     is_active = db.Column(db.Boolean(), default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
